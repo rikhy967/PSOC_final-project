9f3cd5b4762d1e0c0cb356fd287f5894eaaaa556
Simone <simone.toffoli@mail.polimi.it>
2020-05-20 18:23:12 +0200

    added header file for the FIFO registers

--- Final_Project.cydsn/LIS3DH_FIFO_Registers.h
+++ Final_Project.cydsn/LIS3DH_FIFO_Registers.h
@@ -1,13 +1,34 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+//info for ctrl_reg 5
+// brief address of control register 5
+#define LIS3DH_CTRL_REG_5 0x24
+//fifo enable
+#define FIFO_ENABLE 0x40
 
-/* [] END OF FILE */
+
+//info for ctrl_reg 3
+// brief address of control register 3
+#define LIS3DH_CTRL_REG_3 0x22
+//interrupt on fifo watermark
+#define INT_FIFO_WMK 0x04
+
+//interrupt on fifo overrun
+#define INT_FIFO_OVERRUN 0x02
+
+//info for fifo_ctrl_reg
+//brief address of fifo control register
+#define LIS3DH_FIFO_CTRL_REG 0x2E
+//mask for fifo overrun
+#define FIFO_OVR 0x40
+//mask for fifo empty
+#define FIFO_EMPTY 0x20
+
+//info for fifo_src_reg
+// brief address of fifo src reg
+#define LIS3DH_FIFO_SRC_REG_3 0x2F
+
+
+//definition of fifo modes
+#define BYPASS_MODE 0x00
+#define FIFO_MODE 0X40
+#define STREAM_MODE 0x80
+#define STREAM_TO_FIFO_MODE 0xC0
